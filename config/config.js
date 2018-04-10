6ec2cf732b4326d6e94352da71298da86c1af0a5
Deian Vladkov <deian.vladkov@gmail.com>
2018-04-10 20:41:23 +0300

    Basic routes

---
+++ config/config.js
@@ -0,0 +1,10 @@
+// Configuration
+
+module.exports = {
+    cdn: {
+        jQuery: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js',
+        bootstrap: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
+        bootstrapCss: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
+        jQueryValidate: 'https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js'
+    }
+};
---
+++ routes/routes.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+
+
+module.exports = function (app) {
+
+    app.get('/', function (req, res) {
+        res.send('Project index');
+    });
+
+
+    app.get('/error', function (req, res) {
+        res.render('error', {
+            message: req.flash('errorMessage')
+        });
+    });
+
+    // Return 404 on missing pages
+    app.get('*', function (req, res) {
+        res.status(404).send('Error: 404. Page not found !');
+    });
+};
